f2794289327625c9abee5f341e4ada9f0ed58852
Steve <black-balloons@web.de>
2023-05-04 00:07:53 +0200

    added some models

---
+++ CLaunch/CLaunch/Models/CryptocurrencyProfileModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CLaunch.Models
+{
+    public class CryptocurrencyProfileModel
+    {
+        public DateTime MarketOpening { get; set; }
+        public string CurrencyCode { get; set; }
+        public string CurrencyName { get; set; }
+        public int Amount { get; set; }
+        public List<ValueByDayModel> ValueByDay { get; set; }
+    }
+
+    public class ValueByDayModel
+    {
+        public int MedianValue { get; set; }    
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ CLaunch/CLaunch/Models/SellingStrategyModel.cs
@@ -0,0 +1,25 @@
+﻿namespace CLaunch.Models
+{
+    public class SellingStrategyModel
+    {
+        public string Name { get; set; }
+        public List<SellingDateModel> SellingDates { get; set; }
+        public List<SellingMultiplikatorModel> SellingMultiplikators { get; set; }
+    }
+
+    public abstract class SellingModel
+    {
+        public int VolumeInPercent { get; set; }
+    }
+
+
+    public class SellingMultiplikatorModel : SellingModel
+    {
+        public int SellingPointMultiplikator { get; set; }
+    }
+
+    public class SellingDateModel : SellingModel
+    {
+        public DateTime SellingDate { get; set; }
+    }
+}
