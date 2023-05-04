990eeff061f6065186d395e5596174ab9ca5047e
Steve <black-balloons@web.de>
2023-05-05 00:26:52 +0200

    prepared connection to database

---
+++ CLaunch/CLaunch/Controller/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CLaunch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Strategies()
+        {
+            using (SqlConnection connection = new SqlConnection(_config.GetConnectionString("dbConnectionString")))
+            {
+                try
+                {
+                    // connection.Open();
+
+                    return View("Strategies");
+                }
+                catch (Exception ex)
+                {
+                    var bla = ex;
+                    return View("Strategies");
+                }
+            }
+
+        
+        }
+    }
+}
--- CLaunch/CLaunch/Program.cs
+++ CLaunch/CLaunch/Program.cs
@@ -1,7 +1,11 @@
+using CLaunch.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+
 
 var app = builder.Build();
 
@@ -17,6 +21,10 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.UseAuthorization();
 
